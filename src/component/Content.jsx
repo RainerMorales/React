937dc34e65cab1a8b8c526dc658d9a101bf9bc76
RainerMorales <jerenmae212212@gmail.com>
2025-03-20 22:18:00 +0800

    update

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -1,50 +1,44 @@
 import { motion } from "motion/react";
 import SplitText from "../TextAnimations/SplitText/SplitText.jsx";
+import RotatingText from "../TextAnimations/RotatingText/RotatingText.jsx"
+import GradientText from "../TextAnimations/GradientText/GradientText.jsx";
 
 function Content() {
   return (
     <>
-      {/* <div className="flex justify-center items-center h-screen">
-        <SplitText
-          text="tae nayan nagawa ko rin HAHAHAHAHAAHAHAHAHAAHHAHAHA"
-          className="text-2xl font-semibold text-center"
-          delay={150}
-          animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
-          animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-          easing="easeOutCubic"
-          threshold={0.2}
-          rootMargin="-50px"
-
-        />
-      </div> */}
       <motion.main
         initial={{ opacity: 0 }} // Initially hidden
         whileInView={{ opacity: 1 }} // Animate when in view
         transition={{ duration: 1, delay: 0.2 }} // Smooth transition
-        className="pt-25"
+        className="pt-25 max-w-4xl m-auto "
       >
         <div
           id="messages"
           className="text-white fixed w-50 m-auto top-20 left-0 right-0"
         ></div>
-        <div className="max-w-4xl m-auto grid lg:grid-cols-2 sm:grid-cols-1 mb-40 gap-10">
-          <div className="m-auto flex flex-col">
-            <p className=" font-extrabold text-7xl text-white">Hi!</p>
-            <p className=" font-extrabold text-5xl text-white">I'M Rainer</p>
-            <p className=" font-bold text-2xl text-white">
-              Front-End{" "}
-              <motion.span
-                animate={{
-                  backgroundPosition: ["0% 50%", "100% 50%", "0% 50%"],
-                }}
-                transition={{ repeat: Infinity, duration: 3, ease: "linear" }}
-                style={{ backgroundSize: "200% 200%" }}
-                className="bg-gradient-to-r from-blue-600 via-cyan-400 to-teal-500 bg-clip-text text-transparent"
-              >
-                Developer
-              </motion.span>
-              <br />
-            </p>
+        <div className="grid lg:grid-cols-2 sm:grid-cols-1 mb-40 gap-10">
+          <div className="m-auto flex flex-col w-50">
+            <div className=" font-extrabold text-7xl text-white">Hi!</div>
+            <div className="flex items-center ">
+              <div className=" font-extrabold text-5xl text-white">I'M</div>
+              <div className="">
+                <RotatingText
+                  texts={["Rainer", "21"]}
+                  mainClassName="px-2 sm:px-2 md:px-3 font-extrabold text-5xl text-white overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                  staggerFrom={"last"}
+                  initial={{ y: "100%" }}
+                  animate={{ y: 0 }}
+                  exit={{ y: "-120%" }}
+                  staggerDuration={0.025}
+                  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                  transition={{ type: "spring", damping: 30, stiffness: 200 }}
+                  rotationInterval={3000}
+                />
+              </div>
+            </div>
+            <div className=" font-bold text-xl text-white opacity-80">
+              Front-End Developer
+            </div>
             <div className="font-extralight text-lg text-white opacity-80">
               Philippines
             </div>
@@ -77,7 +71,7 @@ function Content() {
           </div>
         </div>
         <div className="p-6 m-2 text-white">
-          <div className="flex gap-2 text-3xl font-bold">
+          <div className="flex justify-center gap-2 text-3xl font-bold">
             About<span className="text-info">Me</span>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-2 mt-10 mb-16">
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         initial={{ y: "-30px", opacity: 0 }} // Initially hidden (opacity 0)
         animate={{ y: "0px", opacity: 1 }} // Final state (opacity 1)
         transition={{ delay: 1, duration: 0.5 }} // Duration of 1 second
-        className="fixed w-full top-0 left-0 p-4 z-50 bg-black shadow-lg shadow-black"
+        className="fixed w-full top-0 left-0 p-4 z-50 bg-black shadow-xl shadow-black"
       >
         <div className="max-w-7xl mx-auto flex justify-between items-center">
           <a href="index.html" className="text-info text-2xl font-bold">
